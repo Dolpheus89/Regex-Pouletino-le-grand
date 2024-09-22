dda1654d79a549d00b8ce693714df81afdcac612
Florian Schaessens <f.schaessens@gmail.com>
2024-09-22 12:46:18 +0200

    initialize and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Pouletino from "../assets/Pouletino.png"
+import Regexlogo from "../assets/regex-tutorial.png"
+
+export default function Header() {
+
+    return (
+        <header>
+            <img src={Pouletino} alt="happy chicken kid" id="pouletino"/>
+            <h1>Les Aventures de <br/> Pouletino le Grand </h1>
+            <img src={Regexlogo} alt="Regex logo" id="regexLogo"/>
+        </header>
+    )
+}
